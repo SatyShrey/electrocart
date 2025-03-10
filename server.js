defd38fde56fb5dcea4a9e6a56ee4b02d88d3002
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-03-10 19:43:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt')
 const path = require('path')
 let PORT = 6060
 app.use(cors({
-    origin: 'https://electrocartatweb3.netlify.app/'
+    origin: 'https://electrocartatweb3.netlify.app'
 }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('.'))
